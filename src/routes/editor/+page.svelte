c9205db8f9877d181093b1169235a29abd23d0b7
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-27 03:25:30 -0500

    add tools to editor

--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,7 +1,69 @@
 <script>
-	import Editor from '$lib/components/Editor.svelte';
+	import { Pencil, EyeDropper, ArrowDownOnSquare, ArrowUpOnSquare, Trash } from 'svelte-heros-v2';
+	import Editor from './Editor.svelte';
+
+	let color;
+	let editor;
 </script>
 
-<main>
-	<Editor />
+<main class="flex h-screen w-screen flex-row items-center justify-center">
+	<div class="flex flex-row gap-3">
+		<!-- okay this is a dirty css workaround but itll do -->
+		<div id="controls" class="flex w-fit flex-col gap-3 rounded-2xl bg-white/30 p-3 shadow-2xl">
+			<input type="color" id="color" bind:this={color} class="m-0 aspect-square h-[5vw] w-[5vw]" />
+
+			<!-- eslint-disable no-alert -->
+			<button
+				class="editor-button bg-white hover:bg-purple-400"
+				on:click={() =>
+					editor.importEditor(prompt('Input your Editor Code')) && editor.paintEditor()}
+			>
+				<ArrowDownOnSquare size="30" />
+			</button>
+			<button
+				class="editor-button bg-white hover:bg-purple-400"
+				on:click={() => alert(`Editor Code:\n${editor.exportEditor()}`)}
+			>
+				<ArrowUpOnSquare size="30" />
+			</button>
+			<!-- eslint-enable no-alert -->
+			<button
+				class="editor-button bg-white {editor.drawingMode ? 'bg-cyan-400' : 'hover:bg-purple-400'}"
+				on:click={() => (editor.drawingMode = !editor.drawingMode)}
+			>
+				<Pencil size="30" />
+			</button>
+
+			<button
+				class="editor-button mt-auto bg-white
+				transition-all duration-300
+				hover:border-4 hover:bg-red-400 hover:text-white"
+				on:click={editor.clearCanvas}
+			>
+				<Trash size="30" />
+			</button>
+		</div>
+		<Editor
+			bind:this={editor}
+			{color}
+			class="aspect-square h-[vh] w-[40vw] rounded-2xl bg-white shadow-2xl"
+		/>
+	</div>
 </main>
+
+<style>
+	input[type='color'] {
+		&::-webkit-color-swatch,
+		&::-webkit-color-swatch-wrapper,
+		& {
+			@apply overflow-hidden rounded-2xl border-none p-0 shadow-lg;
+		}
+
+		&::-moz-color-swatch {
+			@apply border-none;
+		}
+	}
+	.editor-button {
+		@apply flex aspect-square h-[5vw] w-[5vw] items-center justify-center rounded-2xl border-gray-100 shadow-lg;
+	}
+</style>
---
+++ src/routes/editor/Editor.svelte
@@ -0,0 +1,151 @@
+<script>
+	import { onMount } from 'svelte';
+	import { LZMA as lzma } from 'lzma/src/lzma_worker-min';
+
+	export let canvas;
+	export let color;
+	export let drawingMode = true;
+
+	const size = 32;
+	const spacing = 1;
+
+	const generateEditor = () => {
+		const editor = {
+			idToColor: {
+				0: 'clear',
+				1: '#E9BCD1',
+				2: '#34E2E2',
+				3: '#FFFFFF'
+			},
+			board: Array(size)
+				.fill(null)
+				.map(() =>
+					Array(size)
+						.fill(null)
+						.map((cell, i) => (i % 3) + 1)
+				)
+		};
+
+		editor.colorToId = Object.fromEntries(
+			Object.entries(editor.idToColor).map(([key, value]) => [value, key])
+		);
+
+		return editor;
+	};
+
+	let currentEditor = generateEditor();
+
+	let pixelSize;
+	let painting = false;
+	let ctx;
+
+	export const paintEditor = () => {
+		const { board } = currentEditor;
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+		for (let i = 0; i < board.length; i++) {
+			const row = board[i];
+			for (let j = 0; j < row.length; j++) {
+				const cellColorId = row[j];
+				let type = 'fill';
+				if (cellColorId === 'clear') type = cellColorId;
+				else ctx.fillStyle = currentEditor.idToColor[cellColorId];
+
+				ctx[`${type}Rect`](
+					i * pixelSize - spacing,
+					j * pixelSize - spacing,
+					pixelSize - spacing * 2,
+					pixelSize - spacing * 2
+				);
+			}
+		}
+	};
+
+	const debounce = (fn, timeout) => {
+		let timer;
+		return (...args) => {
+			clearTimeout(timer);
+			timer = setTimeout(() => fn.apply(this, args), timeout);
+		};
+	};
+
+	export const exportEditor = () => JSON.stringify(lzma.compress(JSON.stringify(currentEditor), 6));
+	export const importEditor = compressed =>
+		(currentEditor = JSON.parse(lzma.decompress(JSON.parse(compressed))));
+
+	const autoSave = debounce(
+		() => localStorage.setItem('editorAutoSave', exportEditor(currentEditor)),
+		750
+	);
+
+	const activatePainting = () => {
+		painting = true;
+		const selectedColor = color.value;
+		ctx.fillStyle = drawingMode ? selectedColor : 'clear';
+
+		if (!currentEditor.colorToId[selectedColor]) {
+			let previousId = Object.keys(currentEditor.idToColor);
+			previousId = previousId[previousId.length - 1];
+			const nextId = Number(previousId) + 1;
+
+			currentEditor.idToColor[nextId] = selectedColor;
+			currentEditor.colorToId[selectedColor] = nextId;
+		}
+	};
+
+	const deactivatePainting = () => {
+		painting = false;
+		autoSave();
+	};
+
+	const getBoardPosition = pos => (pos - (pos % pixelSize)) / pixelSize;
+
+	const paint = ({ offsetX, offsetY }) => {
+		if (!painting) return;
+		const x = getBoardPosition(offsetX);
+		const y = getBoardPosition(offsetY);
+
+		const startX = x * pixelSize;
+		const startY = y * pixelSize;
+
+		currentEditor.board[x][y] = currentEditor.colorToId[ctx.fillStyle];
+
+		let type = 'fill';
+		if (ctx.fillStyle === 'clear') type = ctx.fillStyle;
+
+		ctx[`${type}Rect`](
+			startX - spacing,
+			startY - spacing,
+			pixelSize - spacing * 2,
+			pixelSize - spacing * 2
+		);
+	};
+
+	export const clearCanvas = () => {
+		if (!ctx) return;
+
+		currentEditor = generateEditor();
+		localStorage.removeItem('editorAutoSave');
+		paintEditor();
+	};
+
+	onMount(() => {
+		ctx = canvas?.getContext('2d');
+
+		canvas.width = canvas.offsetWidth;
+		canvas.height = canvas.offsetHeight;
+		pixelSize = canvas.width / currentEditor.board.length;
+
+		const editorAutoSave = localStorage.getItem('editorAutoSave');
+		if (editorAutoSave) currentEditor = importEditor(editorAutoSave);
+		paintEditor();
+	});
+</script>
+
+<canvas
+	bind:this={canvas}
+	on:mousedown={activatePainting}
+	on:mouseup={deactivatePainting}
+	on:mousemove={paint}
+	{...$$restProps}
+/>
