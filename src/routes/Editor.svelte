a9df47f7d746833ee78ee1754547c55a07ce10a2
CelestialCrafter <celestialcraftermc@gmail.com>
2023-10-29 01:09:25 -0500

    partially do rectangular pixels

--- src/routes/Editor.svelte
+++ src/routes/Editor.svelte
@@ -5,25 +5,28 @@
 	export let canvas;
 	export let color;
 	export let drawingMode = 0;
+	export let spacing = 0;
+
+	export let axisMultiplier = 1.5;
+	export let axis = 'vertical';
+
+	const horizontalMultiplier = Math.max((axis === 'horizontal') * axisMultiplier, 1);
+	const verticalMultiplier = Math.max((axis === 'vertical') * axisMultiplier, 1);
 
 	const size = 32;
 	// @TODO add padding @CelestialCrafter
-	const spacing = 1;
 
 	const generateEditor = () => {
 		const editor = {
 			idToColor: {
-				0: 'clear',
-				1: '#FFF',
-				2: '#FFF',
-				3: '#FFF'
+				0: 'clear'
 			},
 			board: Array(size)
 				.fill(null)
 				.map(() =>
 					Array(size)
 						.fill(null)
-						.map((cell, i) => (i % 3) + 1)
+						.map(() => 0)
 				)
 		};
 
@@ -40,6 +43,13 @@
 	let painting = false;
 	let ctx;
 
+	const calculatePixelPosition = (first, second, type) => {
+		const alternatePixel = type === axis;
+		return alternatePixel
+			? (first + (second % 2)) * (pixelSize * axisMultiplier) - spacing
+			: first * pixelSize - spacing;
+	};
+
 	export const paintEditor = () => {
 		const { board } = currentEditor;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -49,14 +59,14 @@
 			for (let j = 0; j < row.length; j++) {
 				const cellColorId = row[j];
 				let type = 'fill';
-				if (cellColorId === 'clear') type = cellColorId;
+				if (cellColorId === 0) type = 'clear';
 				else ctx.fillStyle = currentEditor.idToColor[cellColorId];
 
 				ctx[`${type}Rect`](
-					i * pixelSize - spacing,
-					j * pixelSize - spacing,
-					pixelSize - spacing * 2,
-					pixelSize - spacing * 2
+					calculatePixelPosition(i, j, 'horizontal'),
+					calculatePixelPosition(j, i, 'vertical'),
+					pixelSize * horizontalMultiplier - spacing * 2,
+					pixelSize * verticalMultiplier - spacing * 2
 				);
 			}
 		}
@@ -110,18 +120,15 @@
 		if (drawingMode === 2 && currentPixel !== 'clear')
 			return (color.value = currentEditor.idToColor[currentPixel]);
 
-		const startX = x * pixelSize;
-		const startY = y * pixelSize;
-
 		currentEditor.board[x][y] = currentEditor.colorToId[ctx.fillStyle];
 
 		const type = drawingMode === 0 ? 'fill' : 'clear';
 
 		ctx[`${type}Rect`](
-			startX - spacing,
-			startY - spacing,
-			pixelSize - spacing * 2,
-			pixelSize - spacing * 2
+			x * pixelSize - spacing,
+			(y + (x % 2)) * pixelSize - spacing,
+			pixelSize * horizontalMultiplier - spacing * 2,
+			pixelSize * verticalMultiplier - spacing * 2
 		);
 	};
 
